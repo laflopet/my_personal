99662231dbbf9caf49ce67d30d140b7c2a9f3cf9
laflopet <laflopet@gmail.com>
2023-05-07 10:07:38 -0500

    Modificacion 2

--- myproject/urls.py
+++ myproject/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from myapp.views import inicio
@@ -29,4 +31,4 @@ urlpatterns = [
     path('eliminar/registros', eliminar_registros, name='eliminar_registros'),
     path('enviar_email', envio_correo, name='enviar_email'),
     path('formulario/', abrir_formulario, name='abrir_formulario')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
